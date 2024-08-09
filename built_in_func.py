6f6adc60a54d5d9b133a9e0ddd6e0cb1b2165cd2
mbrod5 <maya.rene.broda@gmail.com>
2024-08-08 22:38:50 -0500

    working on built in functions

---
+++ built_in_func.py
@@ -0,0 +1,45 @@
+# For Allo 
+
+# sum 
+def sum_allo(iterable, start): 
+    pass
+
+# map 
+def map_allo(): 
+    pass
+
+# list
+def list_allo():
+    pass
+
+# combinations 
+def combinations_allo(): 
+    pass
+
+# len
+def len_allo(): 
+    pass 
+
+# append 
+def append_allo(): 
+    pass
+
+# int 
+def int_allo(): 
+    pass
+
+# floor
+def floor_allo():
+    pass
+
+# log2
+def log2_allo(): 
+    pass
+
+# zip 
+def zip_allo(): 
+    pass 
+
+# tuple 
+def tuple_allo(): 
+    pass 
